6c64297744f350e30aa4fb03c405fc3d1fd5315c
puffluv <alibihasenov@gmail.com>
2024-07-28 16:14:53 +0700

    Add: env variables

--- backend/src/configurations/index.ts
+++ backend/src/configurations/index.ts
@@ -1,3 +1,10 @@
+import { env } from 'process';
+
 export default () => ({
-  port: process.env.PORT,
+  port: env.PORT,
+  db_port: env.DB_PORT,
+  db_name: env.DB_NAME,
+  db_user: env.DB_USER,
+  db_password: env.DB_PASSWORD,
+  db_host: env.DB_HOST,
 });
