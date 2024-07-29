42a39ff8b6146d032e317e5f3dfa7c0769af47e9
puffluv <alibihasenov@gmail.com>
2024-07-30 01:11:19 +0700

    Add: jwt strategy

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { UserModule } from '@modules/user';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TokenModule } from '@modules/token';
+import { JwtStrategy } from '@src/strategy';
 
 
 @Module({
   imports: [UserModule, TokenModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
