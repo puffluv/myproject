9f9785212e996e5917c061fc83a916eb54021530
puffluv <alibihasenov@gmail.com>
2024-07-29 15:46:19 +0700

    Add: create user

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { users } from '@src/moks';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO } from './dto';
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return users;
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
+
+  async hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async createUser(dto): Promise<CreateUserDTO> {
+    dto.password = await this.hashPassword(dto.password);
+    await this.userRepository.create(dto);
+    return dto;
   }
 }
