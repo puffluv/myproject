8c5f8fba2e94aa612aaf3cd2e0b1192307d2edef
puffluv <alibihasenov@gmail.com>
2024-07-30 14:54:41 +0700

    Add: import watchlist model

--- backend/src/modules/watchlist/watchlist.module.ts
+++ backend/src/modules/watchlist/watchlist.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WatchlistController } from './watchlist.controller';
 import { WatchlistService } from './watchlist.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Watchlist } from '@modules/watchlist/models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Watchlist])],
   controllers: [WatchlistController],
-  providers: [WatchlistService]
+  providers: [WatchlistService],
 })
 export class WatchlistModule {}
