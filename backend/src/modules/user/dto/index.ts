512a62d789b392eb37171775a91574d9ad4cc27d
puffluv <alibihasenov@gmail.com>
2024-07-30 13:32:43 +0700

    Add: update user dto

--- backend/src/modules/user/dto/index.ts
+++ backend/src/modules/user/dto/index.ts
@@ -1,20 +1,34 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateUserDTO {
-    @ApiProperty()
-    @IsString()
-    firstName: string
+  @ApiProperty()
+  @IsString()
+  firstName: string;
 
-    @ApiProperty()
-    @IsString()
-    username: string
+  @ApiProperty()
+  @IsString()
+  username: string;
 
-    @ApiProperty()
-    @IsString()
-    email: string
+  @ApiProperty()
+  @IsString()
+  email: string;
 
-    @ApiProperty()
-    @IsString()
-    password: string
+  @ApiProperty()
+  @IsString()
+  password: string;
+}
+
+export class UpdateUserDTO {
+  @ApiProperty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsString()
+  email: string;
 }
