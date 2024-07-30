8533fbdf23167e227de6160f77178110e044c616
puffluv <alibihasenov@gmail.com>
2024-07-30 12:47:27 +0700

    Add: verify refresh and refresh access tokens

--- backend/src/modules/token/token.service.ts
+++ backend/src/modules/token/token.service.ts
@@ -4,15 +4,37 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class TokenService {
-    constructor(private readonly jwtService: JwtService,
-        private readonly configService: ConfigService
-    ) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
-    async generateJwtToken(user) {
-        const payload = {user};
-        return this.jwtService.sign(payload, {
-            secret: this.configService.get('secret_jwt'),
-            expiresIn: this.configService.get('expire_jwt')
-        });
+  async generateJwtToken(user) {
+    const payload = { user };
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('secret_jwt'),
+      expiresIn: Number(this.configService.get<number>('expire_jwt')), // Убедитесь, что это число
+    });
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('refresh_secret'),
+      expiresIn: Number(this.configService.get<number>('expire_refresh_jwt')), // Убедитесь, что это число
+    });
+    return { accessToken, refreshToken };
+  }
+
+  async verifyRefreshToken(token: string) {
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.configService.get('refresh_secret'),
+      });
+      return payload;
+    } catch (e) {
+      throw new Error('Invalid refresh token');
     }
+  }
+
+  async refreshAccessToken(refreshToken: string) {
+    const payload = await this.verifyRefreshToken(refreshToken);
+    return this.generateJwtToken(payload.user);
+  }
 }
