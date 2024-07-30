b0d575fabeaa21beee2302366c854d8be1fcfda7
puffluv <alibihasenov@gmail.com>
2024-07-30 12:46:45 +0700

    Add: token error

--- backend/src/common/constants/errors.ts
+++ backend/src/common/constants/errors.ts
@@ -2,4 +2,5 @@ export const appError = {
   USER_EXIST: 'User with this email exist!',
   USER_NOT_EXIST: 'User with this email not exist!',
   WRONG_DATA: 'Invalid password or email!',
+  INVALID_REF_TOKEN: 'Invalid refresh token!',
 };
