2ffa95681306f79d302d422beec17cbc413d2281
puffluv <alibihasenov@gmail.com>
2024-08-02 20:28:23 +0700

    Add: private route component

---
+++ frontend/src/utils/router/privateRoute.tsx
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const auth = false;
+  return auth ? <Outlet /> : <Navigate to="login" />;
+};
+
+export default PrivateRoute;
