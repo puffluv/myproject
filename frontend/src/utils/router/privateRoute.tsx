cb07d762340a2dec2191793d2e0ac4d1ea18ea19
puffluv <alibihasenov@gmail.com>
2024-08-03 18:27:20 +0700

    Prettier fixed

--- frontend/src/utils/router/privateRoute.tsx
+++ frontend/src/utils/router/privateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
-  const auth = false;
+  const auth = true;
   return auth ? <Outlet /> : <Navigate to="login" />;
 };
 
