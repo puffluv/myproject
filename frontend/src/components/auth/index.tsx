cb3d57b0c03648089139ac2675a3d592f4d1b728
puffluv <alibihasenov@gmail.com>
2024-08-05 02:21:34 +0700

    Add: app errors and react toastify

--- frontend/src/components/auth/index.tsx
+++ frontend/src/components/auth/index.tsx
@@ -7,6 +7,10 @@ import { Box } from "@mui/material";
 import { instance } from "../../utils/axios";
 import { useAppDispatch } from "../../utils/hook";
 import { login } from "../../store/slice/auth";
+import { appErrors } from "../../common/errors";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { AxiosError } from "axios";
 
 const AuthRootComponent: React.FC = (): JSX.Element => {
   const [email, setEmail] = useState("");
@@ -30,30 +34,47 @@ const AuthRootComponent: React.FC = (): JSX.Element => {
         await dispatch(login(user.data))
         navigate('/')
       } catch (error) {
-        console.error("Error logging in", error);
+        if (error instanceof AxiosError) {
+          console.error("Error logging in", error);
+          toast.error(error.response?.data?.message || "Error logging in");
+        }
       }
     } else {
       if (password === confirmPassword) {
-        const userData = {
-          firstName,
-          username,
-          email,
-          password,
-        };
         try {
+          const userData = {
+            firstName,
+            username,
+            email,
+            password,
+          };
           const newUser = await instance.post("auth/register", userData);
-          console.log(newUser.data);
+          await dispatch(login(newUser.data))
+          navigate('/')
         } catch (error) {
-          console.error("Error registering in", error);
+          if (error instanceof AxiosError) {
+            if (error.response) {
+              if (error.response.status === 409) {
+                toast.error("User with this email already exists");
+              } else {
+                toast.error(error.response.data?.message || "Error registering");
+              }
+            } else {
+              console.error("Error registering", error);
+              toast.error("Error registering");
+            }
+          }
         }
       } else {
-        throw new Error("Пароли не совпадают!");
+        toast.error(appErrors.PasswordDoNotMuch);
       }
+
     }
   };
 
   return (
     <div className="root">
+      <ToastContainer />
       <form className="form" onSubmit={handleSubmit}>
         <Box
           display="flex"
@@ -67,7 +88,7 @@ const AuthRootComponent: React.FC = (): JSX.Element => {
           boxShadow={"5px 5px 10px #ccc"}
         >
           {location.pathname === "/login" ? (
-            <LoginPage setEmail={setEmail} setPassword={setPassword} />
+            <LoginPage setEmail={setEmail} setPassword={setPassword} navigate={navigate} />
           ) : location.pathname === "/register" ? (
             <RegisterPage
               setEmail={setEmail}
@@ -75,6 +96,7 @@ const AuthRootComponent: React.FC = (): JSX.Element => {
               setConfirmPassword={setConfirmPassword}
               setFirstName={setFirstName}
               setUsername={setUsername}
+              navigate={navigate}
             />
           ) : null}
         </Box>
