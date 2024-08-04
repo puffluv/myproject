e3d12dfe04c5f7ede368adcc570999e88736bb7a
puffluv <alibihasenov@gmail.com>
2024-08-05 02:14:09 +0700

    Add: auth state, public user and watchlist interfaces

--- frontend/src/common/types/auth/index.ts
+++ frontend/src/common/types/auth/index.ts
@@ -1,6 +1,7 @@
 export interface IPropsLogin {
   setPassword: (value: string) => void;
   setEmail: (value: string) => void;
+  navigate: (to: string) => void;
 }
 
 export interface IPropsRegister {
@@ -9,4 +10,29 @@ export interface IPropsRegister {
   setConfirmPassword: (value: string) => void;
   setFirstName: (value: string) => void;
   setUsername: (value: string) => void;
+  navigate: (to: string) => void;
+}
+
+export interface IAuthState {
+  user: {} | IPublicUser,
+  isLogged: boolean
+}
+
+interface IPublicUser {
+  id: number | null, 
+  firstName: string,
+  username: string,
+  email: string,
+  createdAt: string,
+  updateAt: string,
+  watchlist: [IWatchlist]
+}
+
+interface IWatchlist {
+  id: number | null, 
+  name: string,
+  assetId: string,
+  createdAt: string,
+  updateAt: string,
+  user: number | null
 }
