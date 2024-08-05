3f30fcf519c6dd84a0a06f00227a9945b728870c
puffluv <alibihasenov@gmail.com>
2024-08-05 20:24:19 +0700

    Add: find user by username

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -19,33 +19,44 @@ export class UserService {
     try {
       return bcrypt.hash(password, 10);
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
   async findUserByEmail(email: string): Promise<User> {
     try {
-      return this.userRepository.findOne({ where: { email }, include: {
-        model: Watchlist,
-        required: false,
-      } });
+      return this.userRepository.findOne({
+        where: { email },
+        include: {
+          model: Watchlist,
+          required: false,
+        },
+      });
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
+    }
+  }
+
+  async findUserByUsername(username: string): Promise<User | null> {
+    try {
+      return await this.userRepository.findOne({ where: { username } });
+    } catch (e) {
+      throw new Error(e.message);
     }
   }
 
   async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
     try {
       dto.password = await this.hashPassword(dto.password);
-    await this.userRepository.create({
-      firstName: dto.firstName,
-      username: dto.username,
-      email: dto.email,
-      password: dto.password,
-    });
-    return dto;
+      await this.userRepository.create({
+        firstName: dto.firstName,
+        username: dto.username,
+        email: dto.email,
+        password: dto.password,
+      });
+      return dto;
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
@@ -59,32 +70,32 @@ export class UserService {
           required: false,
         },
       });
-      const tokens = await this.tokenService.generateJwtToken(user)
+      const tokens = await this.tokenService.generateJwtToken(user);
       return {
         user,
         accessToken: tokens.accessToken,
         refreshToken: tokens.refreshToken,
       };
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
   async updateUser(email: string, dto: UpdateUserDTO): Promise<UpdateUserDTO> {
     try {
-    await this.userRepository.update(dto, { where: { email } });
-    return dto;
+      await this.userRepository.update(dto, { where: { email } });
+      return dto;
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 
   async deleteUser(email: string): Promise<boolean> {
     try {
-    const result = await this.userRepository.destroy({ where: { email } });
-    return result > 0;
+      const result = await this.userRepository.destroy({ where: { email } });
+      return result > 0;
     } catch (e) {
-      throw new Error(e)
+      throw new Error(e);
     }
   }
 }
