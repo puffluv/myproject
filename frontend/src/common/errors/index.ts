401df20e1a607bd5bfb6460e3c3f18b0ad43b73b
puffluv <alibihasenov@gmail.com>
2024-08-05 20:23:54 +0700

    Add: errors

--- frontend/src/common/errors/index.ts
+++ frontend/src/common/errors/index.ts
@@ -1,3 +1,8 @@
 export const appErrors = {
-    'PasswordDoNotMuch': 'Пароли не совпадают!'
-}
+  PASSWORD_DONT_MUCH: "Пароли не совпадают!",
+  PASSWORD_LESS: "Пароль должен быть больше, чем 6 символов!",
+  PASSWORD_CHARACTER: "Пароль не должен содержать специальные символы!",
+  INVALID_EMAIL: "Неправильный формат email",
+  ALL_FIELDS_REQUIRED: "All fields are required",
+  EMAIL_OR_USERNAME: "User with this email or username already exists",
+};
