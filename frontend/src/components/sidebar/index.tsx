8c900e83d38176bba96c4c7e9a84689871abdcd2
puffluv <alibihasenov@gmail.com>
2024-08-07 00:53:12 +0700

    Add: sidebar component

---
+++ frontend/src/components/sidebar/index.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { useStyles } from "./styles";
+import {
+  Box,
+  Drawer,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  ChevronLeftOutlined,
+  ChevronRightOutlined,
+  LogoutOutlined,
+} from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "../flex-between";
+import { navMenu } from "../../common/moks/navigate";
+
+const SideBarComponent = (props: any) => {
+  const [active, setActive] = useState("");
+  const { isNotMobile, drawerWidth, isOpen, setIsOpen } = props;
+  const classes = useStyles();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+
+  const renderNavMenu = navMenu.map((element): JSX.Element => {
+    return (
+      <ListItem key={element.id}>
+        <ListItemButton onClick={() => navigate(`${element.path}`)}>
+          <ListItemIcon>{element.icon}</ListItemIcon>
+          <ListItemText>
+            <Typography variant={"body1"}>{element.name}</Typography>
+          </ListItemText>
+        </ListItemButton>
+      </ListItem>
+    );
+  });
+
+  return (
+    <Box component="nav">
+      {isOpen && (
+        <Drawer
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary.main,
+              backgroundColor: theme.palette.primary.main,
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Box width="100%">
+            <Box>
+              <FlexBetween>
+                <Box display="flex" alignItems="center" gap="10px">
+                  <Typography>Myproject</Typography>
+                </Box>
+                {!isNotMobile && (
+                  <IconButton onClick={() => setIsOpen(!isOpen)}>
+                    <ChevronLeftOutlined />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+
+            <List>{renderNavMenu}</List>
+          </Box>
+        </Drawer>
+      )}
+    </Box>
+  );
+};
+
+export default SideBarComponent;
