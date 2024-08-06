75bb12a19b4457e1fca0ff2de8a732cc0f480918
puffluv <alibihasenov@gmail.com>
2024-08-07 00:50:33 +0700

    Add: navigate moks

---
+++ frontend/src/common/moks/navigate/index.tsx
@@ -0,0 +1,34 @@
+import {
+  HomeOutlined,
+  BookmarkBorder,
+  Newspaper,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import path from "path";
+
+export const navMenu = [
+  {
+    name: "Главная",
+    icon: <HomeOutlined />,
+    path: "/",
+    id: 1,
+  },
+  {
+    name: "Избранное",
+    icon: <BookmarkBorder />,
+    path: "/watchlist",
+    id: 2,
+  },
+  {
+    name: "Новости",
+    icon: <Newspaper />,
+    path: "/news",
+    id: 3,
+  },
+  {
+    name: "Настройки",
+    icon: <SettingsOutlined />,
+    path: "/settings",
+    id: 4,
+  },
+];
